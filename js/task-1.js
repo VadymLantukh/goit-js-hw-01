6bf106fe52bb255135bea495175eb40f22f18aab
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-06-12 00:03:11 +0300

    Fix

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
-function makeTransaction(quantity, pricePerDroid) { 
-    return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
-};
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
+}
 
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(5, 1000));
--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,10 @@
-function getElementWidth(content, padding, border) { 
-    const contentSize = Number.parseFloat(content);
-    const paddingSize = Number.parseFloat(padding) * 2;
-    const borderSize = Number.parseFloat(border) * 2;
+function getElementWidth(content, padding, border) {
+  const contentSize = Number.parseFloat(content);
+  const paddingSize = Number.parseFloat(padding) * 2;
+  const borderSize = Number.parseFloat(border) * 2;
 
-    return contentSize + paddingSize + borderSize;
-};
+  return contentSize + paddingSize + borderSize;
+}
 
 console.log(getElementWidth('50px', '8px', '4px'));
 console.log(getElementWidth('60px', '12px', '8.5px'));
