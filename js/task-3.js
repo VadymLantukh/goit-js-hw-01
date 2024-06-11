61bd15c561379cb3dd47fddb22fa0eefedca02e3
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-06-11 23:10:20 +0300

    Add task HW

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) { 
+    const contentSize = Number.parseFloat(content);
+    const paddingSize = Number.parseFloat(padding) * 2;
+    const borderSize = Number.parseFloat(border) * 2;
+
+    return contentSize + paddingSize + borderSize;
+};
+
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
